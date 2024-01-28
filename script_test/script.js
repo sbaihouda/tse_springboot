ee191ec753b9f584738c68ea8d60a492a5f13175
sbai.houda <houdasbai67@gmail.com>
2024-01-28 16:36:28 +0100

    added some CRUD ops in the script

--- script_test/script.js
+++ script_test/script.js
@@ -42,11 +42,53 @@ async function getAllJoueurs() {
         console.error(error);
     }
 }
-async function getJoueur() {
+async function getAllEquipes() {
     try {
-        const response = await axios.get(`${baseUrl}/joueurs/4`);
+        const response = await axios.get(`${baseUrl}/equipes`);
+        console.log("Get toutes les équipes");
+        const listeJoueurs = JSON.stringify(response.data, (key, value) => {
+            if (key === "joueurs" && Array.isArray(value)) {
+                return value.map(player => ({
+                    id: player.id,
+                    nom: player.nom,
+                    prenom: player.prenom,
+                    dateDeNaissance: player.dateDeNaissance,
+                }));
+            }
+            return value;
+        }, 2);
+        console.log(listeJoueurs);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getJoueur(joueurId) {
+    try {
+        const response = await axios.get(`${baseUrl}/joueurs/${joueurId}`);
         console.log("Get joueur 4")
         console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getEquipe(equipeId) {
+    try {
+        const response = await axios.get(`${baseUrl}/equipes/${equipeId}`);
+        const listeJoueurs = JSON.stringify(response.data, (key, value) => {
+            if (key === "joueurs" && Array.isArray(value)) {
+                return value.map(player => ({
+                    id: player.id,
+                    nom: player.nom,
+                    prenom: player.prenom,
+                    dateDeNaissance: player.dateDeNaissance,
+                }));
+            }
+            return value;
+        }, 2);
+        console.log("Get equipe ",equipeId)
+        console.log(listeJoueurs);
     } catch (error) {
         console.error(error);
     }
@@ -56,6 +98,16 @@ async function deleteJoueur() {
     try {
         const response = await axios.delete(`${baseUrl}/joueurs/7`);
         console.log("suppression joueur 7")
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function deleteEquipe(equipeId) {
+    try {
+        const response = await axios.delete(`${baseUrl}/equipes/${equipeId}`);
+        console.log("suppression equipe 2")
         return response.data;
     } catch (error) {
         console.error(error);
@@ -97,8 +149,12 @@ async function main() {
         await createJoueur("Thomas","Monconduit","10/02/1991",ASSE.id);
         // get tout les joueurs
         await getAllJoueurs();
+        //get toutes les equipes
+        await getAllEquipes();
         // get joueur
-        await getJoueur();
+        await getJoueur(4);
+        // get equipe 2
+        await getEquipe(2)
         // suppression du joueur dont id = 7
         await deleteJoueur();
         // modification du joueur dont id = 2
@@ -106,6 +162,10 @@ async function main() {
         // get tout les joueurs apres modifs
         console.log("liste finale après suppression et modifs : ")
         await getAllJoueurs()
+        // suppression equipe 2
+        await deleteEquipe(2)
+        // get toutes mes equipes
+        await getAllEquipes()
 
     } catch (error) {
         console.error("Une erreur s'est produite dans le script principal:", error.message);
